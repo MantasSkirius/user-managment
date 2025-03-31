4946b47661525123721bd1e54c39461694bea0a1
MantasSkirius <mantas.skirius@sa.stud.vu.lt>
2025-03-31 19:16:54 +0300

    Added printing to databaseFile.txt to the abstract user constructor

--- App/Core/AbstractUser.php
+++ App/Core/AbstractUser.php
@@ -1,21 +1,39 @@
 <?php
 namespace App\Core;
+
 abstract class AbstractUser {
-protected $name;
-protected $email;
-protected $password;
-public function __construct($name, $email, $password) {
-$this->name = $name;
-$this->email = $email;
-$this->password = password_hash($password, PASSWORD_DEFAULT); //Hashing password
-}
-public function getName() {
-return $this->name;
-}
-public function getEmail() {
-return $this->email;
-}
-// Force child classes to implement userRole()
-abstract public function userRole();
+    protected $databaseFile;
+    protected $name;
+    protected $email;
+    protected $password;
+    
+    public function __construct($name, $email, $password) {
+        $this->databaseFile = fopen("databaseFile.txt", "a"); 
+        
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = password_hash($password, PASSWORD_DEFAULT); // Hashing password
+        
+        $userData = sprintf(
+            "Name: %s, Email: %s, Password: %s\n",
+            $this->name,
+            $this->email,
+            $this->password
+        );
+        
+        fwrite($this->databaseFile, $userData);
+        fclose($this->databaseFile);
+    }
+    
+    public function getName() {
+        return $this->name;
+    }
+    
+    public function getEmail() {
+        return $this->email;
+    }
+    
+    // Force child classes to implement userRole()
+    abstract public function userRole();
 }
 ?>
