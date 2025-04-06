4e78a0d9d9a8d297ceea2ae064db18f8fac54635
MantasSkirius <mantas.skirius@sa.stud.vu.lt>
2025-04-06 20:45:12 +0300

    Į AbstractUser klasė pridėtas ikėlimo į duomenų bazę metodas, kuris sukurtą vartotoją prideda prie duomenų bazės

--- App/Core/AbstractUser.php
+++ App/Core/AbstractUser.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Core;
+use PDO;
 abstract class AbstractUser {
 protected $name;
 protected $email;
@@ -8,6 +9,7 @@ public function __construct($name, $email, $password) {
 $this->name = $name;
 $this->email = $email;
 $this->password = password_hash($password, PASSWORD_DEFAULT); //Hashing password
+$this->saveToDatabase();
 }
 public function getName() {
 return $this->name;
@@ -15,6 +17,22 @@ return $this->name;
 public function getEmail() {
 return $this->email;
 }
+
+private function saveToDatabase(){
+  $adress="localhost";
+	$user= "root";
+	$pass= "";
+	$database="2skirius_db";
+  $conn = new PDO("mysql:host=$adress;dbname=$database", $user, $pass);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $email=$this->email;
+	$name=$this->name;
+	$password=$this->password;
+	//print ($email." ");
+	$sql="INSERT INTO users (email, name, password) VALUES ('".$email."','".$name."','".$password."')";
+  $conn->exec($sql);
+}
+
 // Force child classes to implement userRole()
 abstract public function userRole();
 }
